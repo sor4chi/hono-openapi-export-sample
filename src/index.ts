0a89b7ad5d941af893f0bd245e30c4c9411f81dc
Sor4chi <80559385+sor4chi@users.noreply.github.com>
2024-12-08 21:28:22 +0900

    feat: add example swagger-ui app

--- src/index.ts
+++ src/index.ts
@@ -1,16 +1,76 @@
-import { serve } from '@hono/node-server'
-import { Hono } from 'hono'
+import { serve } from "@hono/node-server";
+import { z } from "@hono/zod-openapi";
+import { createRoute } from "@hono/zod-openapi";
+import { OpenAPIHono } from "@hono/zod-openapi";
 
-const app = new Hono()
+const ParamsSchema = z.object({
+  id: z
+    .string()
+    .min(3)
+    .openapi({
+      param: {
+        name: "id",
+        in: "path",
+      },
+      example: "1212121",
+    }),
+});
 
-app.get('/', (c) => {
-  return c.text('Hello Hono!')
-})
+const UserSchema = z
+  .object({
+    id: z.string().openapi({
+      example: "123",
+    }),
+    name: z.string().openapi({
+      example: "John Doe",
+    }),
+    age: z.number().openapi({
+      example: 42,
+    }),
+  })
+  .openapi("User");
 
-const port = 3000
-console.log(`Server is running on http://localhost:${port}`)
+const app = new OpenAPIHono();
+
+const route = createRoute({
+  method: "get",
+  path: "/users/{id}",
+  request: {
+    params: ParamsSchema,
+  },
+  responses: {
+    200: {
+      content: {
+        "application/json": {
+          schema: UserSchema,
+        },
+      },
+      description: "Retrieve the user",
+    },
+  },
+});
+
+app.openapi(route, (c) => {
+  const { id } = c.req.valid("param");
+  return c.json({
+    id,
+    age: 20,
+    name: "Ultra-man",
+  });
+});
+
+app.doc("/doc", {
+  openapi: "3.0.0",
+  info: {
+    version: "1.0.0",
+    title: "My API",
+  },
+});
+
+const port = 3000;
+console.log(`Server is running on http://localhost:${port}`);
 
 serve({
   fetch: app.fetch,
-  port
-})
+  port,
+});
